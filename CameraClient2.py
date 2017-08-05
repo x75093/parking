07506e1b58fc381f450b7b4bf4a95375f2d94b21
x75093 <x75093@users.noreply.github.com>
2017-08-04 22:38:31 -0400

    Update CameraClient2.py

--- CameraClient2.py
+++ CameraClient2.py
@@ -5,7 +5,6 @@ import os
 import subprocess                                                               
 from PIL import Image                                                           
 from datetime import datetime 
-#test
                                                                                 
 class CameraClient(object):                                                     
                                                                                 
