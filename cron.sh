c7e2b66abf24d16b28ff870c0db22f7d982f99fe
x75093 <x75093>
2017-08-16 22:31:59 +0000

    test

--- cron.sh
+++ cron.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-/usr/bin/python  /home/pi/Desktop/workbench/parking/CameraClient2.py
+/usr/bin/python /home/pi/Desktop/workbench/parking/CameraClient0.py
+/usr/bin/python /home/pi/Desktop/workbench/parking/CameraClient1.py
+/usr/bin/python /home/pi/Desktop/workbench/parking/CameraClient2.py
+cd /home/pi/Desktop/workbench/parking && git pull -q origin master
 sudo reboot
