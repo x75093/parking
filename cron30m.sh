07421feccdc5ddb1d2489590fc6b703b963a854b
Patrick Kuiper <patrick.kendal.kuiper@gmail.com>
2017-11-01 18:15:26 -0400

    take out reboot

--- cron30m.sh
+++ cron30m.sh
@@ -3,4 +3,4 @@
 /usr/bin/python /home/pi/Desktop/workbench/parking/CameraClient1.py
 /usr/bin/python /home/pi/Desktop/workbench/parking/CameraClient2.py
 cd /home/pi/Desktop/workbench/parking && git pull -q origin master
-sudo reboot
+#sudo reboot
